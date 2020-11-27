1a08f0fecffc11c1a6aa82a75f4f0bb14c9b2e10
Kelly Smith <kdalton77@hotmail.com>
2020-11-27 12:02:28 -0500

    updated html routes

--- Routes/htmlRoute.js
+++ Routes/htmlRoute.js
@@ -5,10 +5,10 @@ var path = require("path");
 //routes to show html content when user visits a page
 module.exports = function(app) {
     app.get("/notes", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/js/notes.html"));
+        res.sendFile(path.join(__dirname, "../public/assets/js/notes.html"));
     });
     
     app.get("/notes", function(req, res) {
-        res.sendFile(path.join(__dirname, "..public/js/index.html"));
+        res.sendFile(path.join(__dirname, "..public/assets/js/index.html"));
     });
 }
