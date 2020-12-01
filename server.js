56f273b4e75b71c07ad3e50e084f21cc4ce72eb4
Kelly Smith <kdalton77@hotmail.com>
2020-11-30 22:38:44 -0500

    link css to index and notes html files

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.status("public"));
+app.use(express.static("public"));
 app.use("/", htmlRoutes);
 app.use("/api", apiRoutes);
 
 app.listen(PORT, () => {
-    console.log('listening at port' + PORT)
+    console.log(`listening on port + http://localhost:${PORT}`)
 });
